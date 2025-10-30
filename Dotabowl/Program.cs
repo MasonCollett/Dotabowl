2e407535d0e70d26e4ffc6fb3ec2c7807601f1e2
MasonCollett <collett3m@gmail.com>
2025-10-30 13:13:43 -0700

    I am once again asking program.cs to work

--- Dotabowl/Program.cs
+++ Dotabowl/Program.cs
@@ -4,6 +4,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+var connectionString = Environment.GetEnvironmentVariable("DefaultConnection")
+                       ?? throw new InvalidOperationException("DefaultConnection not set");
+
+builder.Services.AddDbContext<DotabowlContext>(options =>
+    options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure()));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngular", policy =>
@@ -14,20 +23,16 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddControllers();
-
-// Use the connection string from Configuration/ConnectionStrings
-var connectionString = Environment.GetEnvironmentVariable("DefaultConnection")
-                       ?? throw new InvalidOperationException("DefaultConnection not set");
-
-builder.Services.AddDbContext<DotabowlContext>(options =>
-    options.UseSqlServer(connectionString, sqlOptions => sqlOptions.EnableRetryOnFailure()));
-
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
+
+// Run EF Core migrations
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<DotabowlContext>();
+    db.Database.Migrate();
+    Console.WriteLine("Database migrated successfully.");
+}
+
 app.UseCors("AllowAngular");
 
 // Configure the HTTP request pipeline.
@@ -43,13 +48,4 @@ else
 
 app.UseAuthorization();
 app.MapControllers();
-
-// Run EF Core migrations
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<DotabowlContext>();
-    db.Database.Migrate();
-    Console.WriteLine("Database migrated successfully.");
-}
-
 app.Run();
